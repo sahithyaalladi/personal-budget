33ef18ee8491d73902201e0227729edf97506032
salladi <salladi@uncc.edu>
2024-02-04 12:39:53 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -3,24 +3,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 33
-        },
-        {
-            title: 'Rent',
-            budget: 50
-        },
-        {
-            title: 'Grocery',
-            budget: 90
-        },
-    ]
-};
+
 app.use("/",express.static("public"));
 
+const budget = require("./data.json");
 console.log(budget)
 
 app.get("/hello", (req,res) => {
